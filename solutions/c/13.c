8af68ca1cc317b8ff71bf49ba5419af1a6d310a5
Efim Bogdanovsky <profit234@gmail.com>
2016-12-15 03:14:16 +0200

    Update 13.c

--- solutions/c/13.c
+++ solutions/c/13.c
@@ -1,5 +1,11 @@
 int mod256WithoutMod(int number)
 {
+  /* December 15, 2016
+   * lilsweetcaligula: this code can be further simplified with
+   * a little knowledge of binary math:
+   * 
+   * const int ret = number & 0xFF;
+   */
   static const MOD_VALUE = 256;
   return number - number / MOD_VALUE * MOD_VALUE;
 }
