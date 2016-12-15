c5a67ad8a33fe92fc7a80e168c28efdd063065fc
Efim Bogdanovsky <profit234@gmail.com>
2016-12-15 17:45:45 +0200

    Added comments and marked immutable vars as const.

--- solutions/c/37.c
+++ solutions/c/37.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdlib.h>
 #include <float.h>
 #include <math.h>
@@ -7,19 +8,25 @@ int compareIntegers(const int *lhs, const int *rhs) {
 }
 
 const char* meanVsMedian(int *numbers, size_t numbersSize) {
+  // Make sure the count is odd or zero in accordance with the problem spec.
+  assert(numbersSize == 0 || numbersSize % 2);
+  
   if (!numbers || numbersSize == 0) {
     return "";
   }
   
+  // Sorting is done in-place. While it would generally be better to create
+  // a copy of the numbers array, the spec allows to operate directly on
+  // the numbers array.
   (void)qsort(numbers, numbersSize, sizeof(int), compareIntegers);
-  int median = numbers[numbersSize / 2];
+  const int median = numbers[numbersSize / 2];
   
   double sum = 0.0;
   for (size_t index = 0; index < numbersSize; ++index) {
     sum += numbers[index];
   }
-  double mean = sum / numbersSize;
-  
+    
+  const double mean = sum / numbersSize;  
   if (fabs(mean - median) <= DBL_EPSILON) {
     return "same";
   }
